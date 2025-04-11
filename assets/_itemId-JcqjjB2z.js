8f9a09f4e490f4d658f6542ab1a08ecadb7f60f6
havana723 <havana723@users.noreply.github.com>
2025-04-11 13:04:47 +0000

    deploy: 37da0a3c98e1e1537fd1c6d74c016e059736e63a

--- assets/_itemId-JcqjjB2z.js
+++ assets/_itemId-JcqjjB2z.js
@@ -1,4 +1,4 @@
-import{r,j as e,n as t,R as k}from"./index-OePNyDyu.js";import{g as C,L as I}from"./Layout-D5q7xA8F.js";/**
+import{r,j as e,n,R as k}from"./index-Defs_FbE.js";import{g as C,L as I}from"./Layout-Btk3AdRl.js";/**
  * @license @tabler/icons-react v3.31.0 - MIT
  *
  * This source code is licensed under the MIT license.
@@ -8,7 +8,7 @@ import{r,j as e,n as t,R as k}from"./index-OePNyDyu.js";import{g as C,L as I}fro
  *
  * This source code is licensed under the MIT license.
  * See the LICENSE file in the root directory of this source tree.
- */const g=(o,i,n,s)=>{const c=r.forwardRef(({color:m="currentColor",size:l=24,stroke:v=2,title:d,className:f,children:a,...w},u)=>r.createElement("svg",{ref:u,...M[o],width:l,height:l,className:["tabler-icon",`tabler-icon-${i}`,f].join(" "),strokeWidth:v,stroke:m,...w},[d&&r.createElement("title",{key:"svg-title"},d),...s.map(([j,y])=>r.createElement(j,y)),...Array.isArray(a)?a:[a]]));return c.displayName=`${n}`,c};/**
+ */const g=(o,i,t,s)=>{const c=r.forwardRef(({color:m="currentColor",size:l=24,stroke:v=2,title:d,className:f,children:a,...w},u)=>r.createElement("svg",{ref:u,...M[o],width:l,height:l,className:["tabler-icon",`tabler-icon-${i}`,f].join(" "),strokeWidth:v,stroke:m,...w},[d&&r.createElement("title",{key:"svg-title"},d),...s.map(([j,y])=>r.createElement(j,y)),...Array.isArray(a)?a:[a]]));return c.displayName=`${t}`,c};/**
  * @license @tabler/icons-react v3.31.0 - MIT
  *
  * This source code is licensed under the MIT license.
@@ -18,7 +18,7 @@ import{r,j as e,n as t,R as k}from"./index-OePNyDyu.js";import{g as C,L as I}fro
  *
  * This source code is licensed under the MIT license.
  * See the LICENSE file in the root directory of this source tree.
- */var z=g("outline","user","IconUser",[["path",{d:"M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0",key:"svg-0"}],["path",{d:"M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2",key:"svg-1"}]]);const R=t.div`
+ */var z=g("outline","user","IconUser",[["path",{d:"M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0",key:"svg-0"}],["path",{d:"M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2",key:"svg-1"}]]);const R=n.div`
   width: 100%;
   display: flex;
   align-items: center;
@@ -27,33 +27,33 @@ import{r,j as e,n as t,R as k}from"./index-OePNyDyu.js";import{g as C,L as I}fro
   font-size: 16px;
   font-weight: 300;
   padding-top: calc(50svh - 96px);
-`,E=()=>e.jsx(R,{children:"사진이 존재하지 않습니다."}),L=t.img`
+`,E=()=>e.jsx(R,{children:"사진이 존재하지 않습니다."}),L=n.img`
   width: 100%;
   height: calc(100vh - 96px);
   object-fit: contain;
-`,D=t.div`
+`,D=n.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`,A=t.div`
+`,A=n.div`
   width: 100%;
   max-width: 1200px;
   padding: 64px 2em;
   color: white;
-`,B=t.div`
+`,B=n.div`
   font-weight: 700;
   font-size: 2em;
-`,h=t.div`
+`,h=n.div`
   width: 100%;
   height: 1px;
   border-bottom: 1px dashed #636363;
   margin: 32px 0;
-`,p=t.div`
+`,p=n.div`
   display: flex;
   flex-direction: row;
   align-items: center;
   gap: 16px;
-`,x=t.div`
+`,x=n.div`
   font-weight: 300;
   font-size: 1em;
   color: #c6c6c6;
@@ -61,7 +61,7 @@ import{r,j as e,n as t,R as k}from"./index-OePNyDyu.js";import{g as C,L as I}fro
   flex-direction: row;
   align-items: center;
   gap: 4px;
-`,N=t.div`
+`,N=n.div`
   font-weight: 400;
   font-size: 1em;
   color: #c6c6c6;
@@ -69,4 +69,4 @@ import{r,j as e,n as t,R as k}from"./index-OePNyDyu.js";import{g as C,L as I}fro
   flex-direction: row;
   align-items: center;
   gap: 4px;
-`,S=o=>{const{itemId:i}=o,n=C.find(s=>s.id===i);return n?e.jsxs(e.Fragment,{children:[e.jsx(L,{src:n.image[0]}),e.jsx(D,{children:e.jsxs(A,{children:[e.jsxs(p,{children:[e.jsx(B,{children:n.title})," ",e.jsxs(x,{children:[e.jsx(b,{size:"1.2em"})," ",n.date.toLocaleDateString()]})]}),e.jsx(h,{}),e.jsx(p,{children:e.jsxs(x,{children:[e.jsx(z,{size:"1.2em"})," ",n.author]})}),e.jsx(h,{}),e.jsx(N,{children:n.description})]})})]}):e.jsx(E,{})},P=function(){const{itemId:i}=k.useParams();return e.jsx(I,{children:e.jsx(S,{itemId:i})})};export{P as component};
+`,S=o=>{const{itemId:i}=o,t=C.find(s=>s.id===i);return t?e.jsxs(e.Fragment,{children:[e.jsx(L,{src:t.image[0]}),e.jsx(D,{children:e.jsxs(A,{children:[e.jsxs(p,{children:[e.jsx(B,{children:t.title})," ",e.jsxs(x,{children:[e.jsx(b,{size:"1.2em"})," ",t.date.toLocaleDateString()]})]}),e.jsx(h,{}),t.author&&e.jsx(p,{children:e.jsxs(x,{children:[e.jsx(z,{size:"1.2em"})," ",t.author]})}),e.jsx(h,{}),t.description&&e.jsx(N,{children:t.description})]})})]}):e.jsx(E,{})},P=function(){const{itemId:i}=k.useParams();return e.jsx(I,{children:e.jsx(S,{itemId:i})})};export{P as component};
