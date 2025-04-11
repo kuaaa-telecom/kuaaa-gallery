89c95ceb487eb449b7020b23368dc9fe15de0b00
최희원 <jeeen1@naver.com>
2025-04-11 19:14:37 +0900

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,35 @@
-import styled from "@emotion/styled";
-import React from "react";
-import "./App.css";
-import PosterGrid from "./components/PosterGrid";
-import SpotLight from "./components/SpotLight";
-
-const Background = styled.div`
-  height: 100%;
-  width: 100%;
-  background: black;
-  position: absolute;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-`;
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
     <>
-      <Background>
-        <PosterGrid />
-        <SpotLight />
-      </Background>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
